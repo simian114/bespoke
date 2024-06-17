218b0141867c489fc72e629725e32afeb5aa3057
simian114 <artexi1464.2@gmail.com>
2024-06-17 20:02:03 +0900

    [#5] chg: 설정

--- src/main/java/com/blog/bespoke/BespokeApplication.java
+++ src/main/java/com/blog/bespoke/BespokeApplication.java
@@ -2,12 +2,13 @@ package com.blog.bespoke;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class BespokeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BespokeApplication.class, args);
     }
-
 }
