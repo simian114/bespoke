abf2c3202b027fcbdf5b7f284cced938bfd6d368
simian114 <artexi1464.2@gmail.com>
2024-06-17 12:38:21 +0900

    init

---
+++ src/test/java/com/blog/bespoke/BespokeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blog.bespoke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BespokeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
