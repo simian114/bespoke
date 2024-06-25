bd8aa37aa378d50ed3cbe5f71515ed3fe17e8a6f
Nam_Ssang <simian114@gmail.com>
2024-06-25 10:43:05 +0900

    [#18] add: reissue token

--- src/main/java/com/blog/bespoke/presentation/web/controller/AuthController.java
+++ src/main/java/com/blog/bespoke/presentation/web/controller/AuthController.java
@@ -1,14 +1,12 @@
 package com.blog.bespoke.presentation.web.controller;
 
 import com.blog.bespoke.application.dto.request.LoginRequestDto;
+import com.blog.bespoke.application.dto.request.ReIssueTokenRequestDto;
 import com.blog.bespoke.application.usecase.AuthUseCase;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -20,4 +18,15 @@ public class AuthController {
     public ResponseEntity<?> login(@Valid @RequestBody LoginRequestDto requestDto) {
         return ResponseEntity.ok(authUseCase.login(requestDto));
     }
+
+    @PostMapping("/reissue")
+    public ResponseEntity<?> reIssueToken(@Valid @RequestBody ReIssueTokenRequestDto requestDto) {
+        return ResponseEntity.ok(authUseCase.reIssueToken(requestDto));
+    }
+
+    @GetMapping("/test")
+    public String test() {
+        return "zz";
+    }
+
 }
