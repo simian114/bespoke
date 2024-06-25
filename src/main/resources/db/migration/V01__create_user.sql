5026e428dd8296ae84c5084c75df7fe8c3efccff
Nam_Ssang <simian114@gmail.com>
2024-06-25 10:25:21 +0900

    [#18] add: token schema

--- src/main/resources/db/migration/V01__create_user.sql
+++ src/main/resources/db/migration/V01__create_user.sql
@@ -27,6 +27,17 @@ create table user_role
     foreign key (role_id) references role (role_id) on delete cascade
 );
 
+create table token
+(
+    token_id bigint primary key auto_increment,
+    type varchar(30) not null, # EMAIL, REFRESH_TOKEN
+    ref_id bigint not null,
+    ref_type varchar(30) not null, # USER, POST
+    expired_at timestamp not null,
+    code varchar(36) not null,
+    index idx_code (code) # code 에 index 추가
+);
+
 INSERT INTO role(code)
 values ('USER');
 INSERT INTO role(code)
