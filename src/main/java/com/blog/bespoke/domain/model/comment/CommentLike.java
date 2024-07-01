f1a1f79db9fcb999e135f65e9956e8d6e90655b2
Nam_Ssang <simian114@gmail.com>
2024-07-01 15:30:35 +0900

    [#60] add: post, comment 스키마 추가, entity 생성 및 수정

---
+++ src/main/java/com/blog/bespoke/domain/model/comment/CommentLike.java
@@ -0,0 +1,38 @@
+package com.blog.bespoke.domain.model.comment;
+
+import com.blog.bespoke.domain.model.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+public class CommentLike {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comment_like_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "comment_id")
+    private Comment comment;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @CreatedDate
+    @Column(updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime createdAt;
+}
--- src/main/resources/db/migration/V01__create_user.sql
+++ src/main/resources/db/migration/V01__create_user.sql
@@ -12,6 +12,7 @@ create table users
     deleted_at   timestamp    # 삭제 여부
 );
 
+
 create table role
 (
     role_id bigint primary key auto_increment,
@@ -29,27 +30,89 @@ create table user_role
 
 create table token
 (
-    token_id bigint primary key auto_increment,
-    type varchar(30) not null, # EMAIL, REFRESH_TOKEN
-    ref_id bigint not null,
-    ref_type varchar(30) not null, # USER, POST
-    expired_at timestamp not null,
-    code varchar(36) not null,
-    index idx_code (code) # code 에 index 추가
+    token_id   bigint primary key auto_increment,
+    type       varchar(30) not null, # EMAIL, REFRESH_TOKEN
+    ref_id     bigint      not null,
+    ref_type   varchar(30) not null, # USER, POST
+    expired_at timestamp   not null,
+    code       varchar(36) not null,
+    index idx_code (code)            # code 에 index 추가
 );
 
 create table follow
 (
-    follow_id bigint primary key auto_increment,
+    follow_id    bigint primary key auto_increment,
     following_id bigint not null,
-    follower_id bigint not null,
-    created_at timestamp default current_timestamp,
-    foreign key(following_id) references users(user_id) on delete cascade,
-    foreign key(follower_id) references users(user_id) on delete cascade,
+    follower_id  bigint not null,
+    created_at   timestamp default current_timestamp,
+    foreign key (following_id) references users (user_id) on delete cascade,
+    foreign key (follower_id) references users (user_id) on delete cascade,
     constraint unique_follow unique (following_id, follower_id)
 );
+
+
+create table post
+(
+    post_id     bigint primary key auto_increment,
+
+    title       varchar(100) not null,       # id 기반으로.. user 가 작성한 게시글에서만 unique하면 됨
+    description varchar(200),
+    content     longtext,
+
+    status      varchar(20) default 'DRAFT', # DRAFT, PUBLISHED, BLOCKED
+
+    author_id   bigint       not null,
+
+    # tags varchar(255), # ,로 구분되는 문자열.
+
+    # TODO: category
+    foreign key (author_id) references users (user_id) on delete cascade,
+
+    created_at  timestamp   default current_timestamp,
+    updated_at  timestamp on update current_timestamp,
+    deleted_at  timestamp
+);
+
+create table comment
+(
+    comment_id bigint primary key auto_increment,
+    content    text,
+    post_id    bigint not null,
+    user_id    bigint not null,
+
+    foreign key (post_id) references post (post_id) on delete cascade,
+    foreign key (user_id) references users (user_id) on delete cascade,
+
+    created_at timestamp default current_timestamp,
+    updated_at timestamp on update current_timestamp,
+    deleted_at timestamp
+);
+
+create table post_like
+(
+    post_like_id bigint primary key auto_increment,
+    post_id      bigint not null,
+    user_id      bigint not null,
+    foreign key (post_id) references post (post_id) on delete cascade,
+    foreign key (user_id) references users (user_id) on delete cascade,
+    created_at timestamp default current_timestamp
+);
+
+
+create table comment_like
+(
+    comment_like_id bigint primary key auto_increment,
+    comment_id      bigint not null,
+    user_id         bigint not null,
+    foreign key (comment_id) references comment (comment_id) on delete cascade,
+    foreign key (user_id) references users (user_id) on delete cascade,
+    created_at timestamp default current_timestamp
+);
+
+
 
 INSERT INTO role(code)
 values ('USER');
 INSERT INTO role(code)
 values ('ADMIN');
+
