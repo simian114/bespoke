21dd2eb98d3d7c7ce0395348c4aa25db9d5a278c
Nam_Ssang <simian114@gmail.com>
2024-07-02 16:08:46 +0900

    [#72] chg: post like 로직 수정

--- src/main/java/com/blog/bespoke/infrastructure/repository/post/PostJpaRepository.java
+++ src/main/java/com/blog/bespoke/infrastructure/repository/post/PostJpaRepository.java
@@ -8,6 +8,6 @@ import org.springframework.data.repository.query.Param;
 import java.util.Optional;
 
 public interface PostJpaRepository extends JpaRepository<Post, Long> {
-    @Query("SELECT DISTINCT p FROM Post p JOIN FETCH p.postLikes l WHERE p.id = :postId AND p.status = 'PUBLISHED' AND l.user.id = :userId")
+    @Query("SELECT DISTINCT p FROM Post p LEFT JOIN FETCH p.postLikes l WHERE p.id = :postId AND p.status = 'PUBLISHED' AND l.user.id = :userId")
     Optional<Post> findPostWithLikeByIdAndUserId(@Param("postId") Long postId, @Param("userId") Long userId);
 }
