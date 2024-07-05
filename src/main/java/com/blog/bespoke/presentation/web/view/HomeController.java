cb25ac1d61adf6e4697befa34ba14df9543b3d0e
Nam_Ssang <simian114@gmail.com>
2024-07-05 17:42:17 +0900

    [#65] add: 홈페이지(미완성) 및 기본 레이아웃

---
+++ src/main/java/com/blog/bespoke/presentation/web/view/HomeController.java
@@ -0,0 +1,19 @@
+package com.blog.bespoke.presentation.web.view;
+
+import com.blog.bespoke.domain.model.user.User;
+import com.blog.bespoke.infrastructure.web.argumentResolver.annotation.LoginUser;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping({"", "/", "/home"})
+    public String home(@LoginUser User currentUser, Model model) {
+        // currentUser
+        System.out.println(currentUser);
+        model.addAttribute("user", currentUser);
+        return "page/home/home";
+    }
+    // 공통으로 model 에 user 는 넣기
+}
