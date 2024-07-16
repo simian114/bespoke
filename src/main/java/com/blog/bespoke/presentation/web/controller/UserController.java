bcb8960b110f9dc7f3dbfdfc4e85902630bccdd7
Nam_Ssang <simian114@gmail.com>
2024-07-16 22:03:11 +0900

    [#109] chg: 기존 api controller 빈 등록 해제

--- src/main/java/com/blog/bespoke/presentation/web/controller/UserController.java
+++ src/main/java/com/blog/bespoke/presentation/web/controller/UserController.java
@@ -16,7 +16,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+//@RestController
 @RequestMapping("/api/user")
 @RequiredArgsConstructor
 public class UserController {
@@ -59,7 +59,7 @@ public class UserController {
                                             @PathVariable("categoryId") Long categoryId,
                                             @RequestBody CategoryUpdateCmd cmd,
                                             @LoginUser User currentUser
-                                            ) {
+    ) {
         userCategoryUseCase.updateCategory(categoryId, cmd, currentUser);
         return ResponseEntity.ok("");
     }
--- src/main/java/com/blog/bespoke/presentation/web/view/auth/SignupController.java
+++ src/main/java/com/blog/bespoke/presentation/web/view/auth/SignupController.java
@@ -10,7 +10,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -19,7 +18,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequiredArgsConstructor
-public class SignupPageController {
+public class SignupController {
     private final UserUseCase userUseCase;
 
     @GetMapping("/signup")
