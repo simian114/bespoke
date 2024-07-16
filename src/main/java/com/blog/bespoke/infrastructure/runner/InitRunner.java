b325151ba865260adc968d6a2c7637415aa4f69f
Nam_Ssang <simian114@gmail.com>
2024-07-16 21:55:00 +0900

    [#114] add: 게시글 디테일 페이지 기본 구현

--- src/main/java/com/blog/bespoke/infrastructure/runner/InitRunner.java
+++ src/main/java/com/blog/bespoke/infrastructure/runner/InitRunner.java
@@ -60,7 +60,42 @@ public class InitRunner implements CommandLineRunner {
         try {
             User user = userRepository.getById(userId);
             for (int i = 0; i < count; i++) {
-                PostCreateRequestDto dto = PostCreateRequestDto.builder().title("title").description("description").content("content").status(Post.Status.PUBLISHED).build();
+                PostCreateRequestDto dto = PostCreateRequestDto.builder().title("title").description("description").content("""
+                        <p>안녕하세요 sanam 입니다.</p>
+                        <h2>Bespoke 블로그 프로젝트를 만들게 된 이유</h2>
+                        <p>개인 프로젝트로 블로글 플랫폼을 만들게 되었습니다.&nbsp; 수 많은 주제 가운데 블로그 플랫폼을 선택한 이유는 아래와 같습니다.</p>
+                        <ol>
+                        <li>블로그 플랫폼은 확장이 쉽다..</li>
+                        <li>초반의 기획이 다른 서비스들보다 쉽다.</li>
+                        </ol>
+                        <p>확장이 쉽다라는건, 간단한 블로그지만 알림 / 채팅 / 광고 등 많은 서비스와 결합이 가능하다는 것이고 초반으 기획이 쉽다는 건 단순 블로그이기 때문에 기획이 어렵지 않다는 의미입니다.</p>
+                        <p>즉, 첫 프로젝트로 시작하기에 이만한 주제가 없다고 생각했습니다.</p>
+                        <p>물론 초반이 쉽다고 해서 개발이 쉬워서는 안된다고 생각합니다.</p>
+                        <p>개발 시작 전 해당 프로젝트의 구조를 어떻게 가져갈지에 대해 많은 고민을 했고, 그 결과 <strong>도메인 주도 설계</strong>에 기반한 <strong>layered architecture</strong> 로 개발을 시작하게 되었습니다.</p>
+                        <p>인터넷에 많은 자료가 있지는 않은 구조라 프로젝트 진행 초반에는 어려움이 있었지만만 진행하는 과정에서 구조에 대한 질문을 던지고 그 답변을 해나가는 과정에서도 많은 깨달음이 있었습니다.</p>
+                        <p>그렇게 4단계 구조으 아키텍처가 완성되었고 특정 기술에 종속적이지 않은 코드를 만들 수 있게 되었습니다.</p>
+                        <h2>사용된 기술</h2>
+                        <p>사용된 기술은 크게 백엔드와 프론트엔드 그리고 배포로 나눠볼 수 있습니다.</p>
+                        <p>백엔드 같은 경우는 아래의 기술을 사용했습니다.</p>
+                        <ul>
+                        <li>Spring</li>
+                        <li>Mysql</li>
+                        <li>Redis</li>
+                        <li>RabbitMq</li>
+                        </ul>
+                        <p>프론트엔드는 아래의 기술을 사용했습니다.</p>
+                        <ul>
+                        <li>thymeleaf</li>
+                        <li>htmx</li>
+                        <li>bulma (css framework)</li>
+                        </ul>
+                        <p>저는 프론트엔드 개발자로써 경력이 있기에 react 에 익숙한 상황이었지만 react 에만 너무 몰두한 나머지 웹의 기본을 등한시해버렸습니다.</p>
+                        <p>때문에 이번 프로젝트에서는 react 를 사용하지 않고 spring의 대표 template engine 인 thymeleaf 를 사용하게 되었습니다.</p>
+                        <p>&nbsp;</p>
+                        <p>&nbsp;</p>
+                        <p>&nbsp;</p>
+                        <p>&nbsp;</p>
+                                                """).status(Post.Status.PUBLISHED).build();
                 postUseCase.write(dto, user);
             }
         } catch (Exception e) {
---
+++ src/main/java/com/blog/bespoke/presentation/web/view/blog/PostPageController.java
@@ -0,0 +1,37 @@
+package com.blog.bespoke.presentation.web.view.blog;
+
+import com.blog.bespoke.application.dto.response.UserResponseDto;
+import com.blog.bespoke.application.usecase.post.PostUseCase;
+import com.blog.bespoke.application.usecase.user.UserUseCase;
+import com.blog.bespoke.domain.model.post.Post;
+import com.blog.bespoke.domain.model.user.User;
+import com.blog.bespoke.infrastructure.web.argumentResolver.annotation.LoginUser;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+@RequiredArgsConstructor
+public class PostPageController {
+    private final PostUseCase postUseCase;
+    private final UserUseCase userUseCase;
+
+    @GetMapping("/blog/posts/{postId}")
+    public String postDetailPage(@PathVariable("postId") Long postId,
+                                 @LoginUser User currentUser,
+                                 Model model) {
+        Post post = postUseCase.getPostById(postId);
+        UserResponseDto owner = userUseCase.getUserWithCategoryByNickname(post.getAuthor().getNickname());
+
+        model.addAttribute("me", currentUser);
+        model.addAttribute("isOwner", currentUser != null && currentUser.getNickname().equals(owner.getNickname()));
+        model.addAttribute("post", post);
+        model.addAttribute("owner", owner);
+
+        // TODO: 댓글...
+        // TODO: floating TOC 만들기. 모바일 환경에서는 최상단에 details / summary 형태로 구현
+        return "page/post/post";
+    }
+}
