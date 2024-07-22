33ded9a809f652a7eccad6122ad3a0fd0767e01a
Nam_Ssang <simian114@gmail.com>
2024-07-22 15:18:49 +0900

    [#36] chg: 좋아요 수정 및 토스트

--- src/main/java/com/blog/bespoke/application/usecase/post/PostLikeUseCase.java
+++ src/main/java/com/blog/bespoke/application/usecase/post/PostLikeUseCase.java
@@ -7,6 +7,7 @@ import com.blog.bespoke.application.event.publisher.EventPublisher;
 import com.blog.bespoke.application.exception.BusinessException;
 import com.blog.bespoke.application.exception.ErrorCode;
 import com.blog.bespoke.domain.model.post.Post;
+import com.blog.bespoke.domain.model.post.PostRelation;
 import com.blog.bespoke.domain.model.user.User;
 import com.blog.bespoke.domain.repository.post.PostRepository;
 import jakarta.transaction.Transactional;
@@ -23,19 +24,20 @@ public class PostLikeUseCase {
 
     @Transactional
     public PostResponseDto likePost(Long postId, User currentUser) {
-        Post post = postRepository.getById(postId);
+        PostRelation relation = PostRelation.builder().count(true).build();
+        Post post = postRepository.getById(postId, relation);
         if (postRepository.existsPostLikeByPostIdAndUserId(postId, currentUser.getId())) {
             throw new BusinessException(ErrorCode.ALREADY_LIKE_POST);
         }
 
         post.addLike(currentUser);
         post.setLikedByUser(true);
-        PostResponseDto dto = PostResponseDto.from(post);
+        PostResponseDto dto = PostResponseDto.from(post, relation);
         PostResponseDto.PostCountInfoResponseDto countInfo = dto.getCountInfo();
         countInfo.setLikeCount(countInfo.getLikeCount() + 1);
 
         // 이벤트에서 실제 db값 증가. postCountInfo 와 userCountInfo 둘 다 해야함
-        publisher.publishPostLikeEvent(new PostLikeMessage(currentUser.getId(), currentUser.getNickname(), post.getAuthor().getId(), post.getAuthor().getNickname(),postId, post.getTitle(), LocalDateTime.now()));
+        publisher.publishPostLikeEvent(new PostLikeMessage(currentUser.getId(), currentUser.getNickname(), post.getAuthor().getId(), post.getAuthor().getNickname(), postId, post.getTitle(), LocalDateTime.now()));
         return dto;
     }
 
@@ -47,7 +49,7 @@ public class PostLikeUseCase {
         }
         post.cancelLike(postId, currentUser);
         post.setLikedByUser(false);
-        PostResponseDto dto = PostResponseDto.from(post);
+        PostResponseDto dto = PostResponseDto.from(post, PostRelation.builder().count(true).build());
         PostResponseDto.PostCountInfoResponseDto countInfo = dto.getCountInfo();
         countInfo.setLikeCount(countInfo.getLikeCount() - 1);
         // 이벤트 핸들러에서는 유저의 값 감소 & post 에서는 증가
