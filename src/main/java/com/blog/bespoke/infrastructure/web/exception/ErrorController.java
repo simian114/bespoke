6415bca86ca01bc9a9b4ee96805ffae19cad6178
Nam_Ssang <simian114@gmail.com>
2024-07-22 13:12:32 +0900

    [#108] add: common error page

---
+++ src/main/java/com/blog/bespoke/infrastructure/web/exception/ErrorController.java
@@ -0,0 +1,16 @@
+package com.blog.bespoke.infrastructure.web.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class ErrorController {
+    @ExceptionHandler(Throwable.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public String exception(final Throwable throwable, final Model model) {
+        return "error/error";
+    }
+}
