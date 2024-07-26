96d5d56d04b998c5fa47bedc21e9945e22d4c07a
Nam_Ssang <simian114@gmail.com>
2024-07-26 14:30:18 +0900

    [#76] add: comment usecase

---
+++ src/main/java/com/blog/bespoke/application/usecase/post/PostCommentUseCase.java
@@ -0,0 +1,52 @@
+package com.blog.bespoke.application.usecase.post;
+
+import com.blog.bespoke.application.dto.request.CommentCreateRequestDto;
+import com.blog.bespoke.application.dto.request.CommentUpdateRequestDto;
+import com.blog.bespoke.application.dto.response.CommentResponseDto;
+import com.blog.bespoke.domain.model.comment.Comment;
+import com.blog.bespoke.domain.model.comment.CommentUpdateCmd;
+import com.blog.bespoke.domain.model.post.Post;
+import com.blog.bespoke.domain.repository.comment.CommentRepository;
+import com.blog.bespoke.domain.repository.post.PostRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class PostCommentUseCase {
+    private final PostRepository postRepository;
+    private final CommentRepository commentRepository;
+
+    @Transactional
+    public List<CommentResponseDto> getCommentsByPostId(Long postId) {
+        return commentRepository.getCommentsByPostId(postId)
+                .stream().map(CommentResponseDto::from)
+                .toList();
+    }
+
+    @Transactional
+    public CommentResponseDto addComment(CommentCreateRequestDto requestDto, Long postId) {
+        // exist 확인
+        Post post = postRepository.getById(postId);
+
+        Comment comment = requestDto.toModel();
+        comment.setPost(post);
+        return CommentResponseDto.from(commentRepository.save(comment));
+    }
+
+    @Transactional
+    public void deleteComment(Long commentId) {
+        postRepository.deleteById(commentId);
+    }
+
+    @Transactional
+    public CommentResponseDto updateComment(Long commentId, CommentUpdateRequestDto requestDto, Long postId) {
+        Comment comment = commentRepository.getById(commentId);
+        CommentUpdateCmd cmd = CommentUpdateCmd.builder().content(requestDto.getContent()).build();
+        comment.update(cmd);
+        return CommentResponseDto.from(comment);
+    }
+}
--- src/main/java/com/blog/bespoke/domain/model/comment/Comment.java
+++ src/main/java/com/blog/bespoke/domain/model/comment/Comment.java
@@ -4,8 +4,16 @@ import com.blog.bespoke.domain.model.common.TimeStamp;
 import com.blog.bespoke.domain.model.post.Post;
 import com.blog.bespoke.domain.model.user.User;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@SuperBuilder
 public class Comment extends TimeStamp {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,4 +30,14 @@ public class Comment extends TimeStamp {
     private User user;
 
     private String content;
+
+    public void setPost(Post post) {
+        this.post = post;
+    }
+
+    public void update(CommentUpdateCmd cmd) {
+        if (cmd.getContent() != null && !cmd.getContent().isBlank()) {
+            this.content = cmd.getContent();
+        }
+    }
 }
