30ea3ab0aafac1b06b5fa2c028a49c5c77a54c3d
Nam_Ssang <simian114@gmail.com>
2024-07-29 22:40:19 +0900

    [#67] fix: 에러 페이지 모델값 에러 해결

--- src/main/java/com/blog/bespoke/infrastructure/web/exception/ErrorController.java
+++ src/main/java/com/blog/bespoke/infrastructure/web/exception/ErrorController.java
@@ -1,5 +1,7 @@
 package com.blog.bespoke.infrastructure.web.exception;
 
+import com.blog.bespoke.domain.model.user.User;
+import com.blog.bespoke.infrastructure.web.argumentResolver.annotation.LoginUser;
 import org.springframework.http.HttpStatus;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -10,7 +12,8 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 public class ErrorController {
     @ExceptionHandler(Throwable.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public String exception(final Throwable throwable, final Model model) {
+    public String exception(final Throwable throwable, final Model model, @LoginUser User currentUser) {
+        model.addAttribute("me", currentUser);
         return "error/error";
     }
 }
