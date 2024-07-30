b1299c6408d14d703d3b126b64613da84b7b70fa
Nam_Ssang <simian114@gmail.com>
2024-07-30 17:17:07 +0900

    [#184] add: 정적 파일 캐싱

--- src/main/java/com/blog/bespoke/infrastructure/web/config/WebConfig.java
+++ src/main/java/com/blog/bespoke/infrastructure/web/config/WebConfig.java
@@ -2,15 +2,25 @@ package com.blog.bespoke.infrastructure.web.config;
 
 import com.blog.bespoke.infrastructure.web.argumentResolver.LoginUser;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new LoginUser());
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/", "classpath:/static")
+                .setCacheControl(CacheControl.maxAge(365, TimeUnit.DAYS).cachePublic());
     }
 }
