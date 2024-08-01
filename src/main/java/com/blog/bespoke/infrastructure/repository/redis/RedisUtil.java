6630df8ab7c39ff3318e66998a7a0b04d11940f9
Nam_Ssang <simian114@gmail.com>
2024-08-01 17:00:43 +0900

    [#192] add: redis data 추가 및 게시글 디테일 post 정보 Redis 캐시 적용

--- src/main/java/com/blog/bespoke/infrastructure/repository/redis/RedisUtil.java
+++ src/main/java/com/blog/bespoke/infrastructure/repository/redis/RedisUtil.java
@@ -1,39 +1,46 @@
-//package com.blog.bespoke.infrastructure.repository.redis;
-//
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//import lombok.RequiredArgsConstructor;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.data.redis.core.RedisTemplate;
-//import org.springframework.stereotype.Component;
-//
-//import java.util.concurrent.TimeUnit;
-//
-//@Slf4j(topic = "redis")
-//@Component
-//@RequiredArgsConstructor
-//public class RedisUtil {
-//    private final RedisTemplate<String, Object> redisTemplate;
-//    private final ObjectMapper objectMapper;
-//    private static final Long TtlAsMilliSeconds = (long) (1000 * 60 * 60);
-//
-//    public <T> T get(String key, Class<T> clazz) {
-//        Object object;
-//        try {
-//            object = redisTemplate.opsForValue().get(key);
-//        } catch (Exception e) {
-//            return null;
-//        }
-//        if (object == null) {
-//            return null;
-//        }
-//        return objectMapper.convertValue(object, clazz);
-//    }
-//
-//    public void set(String key, Object value) {
-//        try {
-//            redisTemplate.opsForValue().set(key, value, TtlAsMilliSeconds, TimeUnit.MILLISECONDS);
-//        } catch (Exception e) {
-//            log.error(key, " redis 직렬화 실패", e);
-//        }
-//    }
-//}
+package com.blog.bespoke.infrastructure.repository.redis;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
+
+@Slf4j(topic = "redis")
+@Component
+@RequiredArgsConstructor
+public class RedisUtil {
+    private static final Long TtlAsMilliSeconds = (long) (1000 * 60 * 60);
+    private final RedisTemplate<String, Object> redisTemplate;
+    private final ObjectMapper objectMapper;
+
+    public <T> T get(String key, Class<T> clazz) {
+        try {
+            Object object = redisTemplate.opsForValue().get(key);
+            if (object == null) {
+                return null;
+            }
+            return objectMapper.convertValue(object, clazz);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    public void set(String key, Object value) {
+        templateSet(key, value, TtlAsMilliSeconds, TimeUnit.MILLISECONDS);
+    }
+
+    public void set(String key, Object value, Long ttlAsMinute) {
+        templateSet(key, value, ttlAsMinute, TimeUnit.MINUTES);
+    }
+
+    private void templateSet(String key, Object value, Long ttl, TimeUnit unit) {
+        try {
+            redisTemplate.opsForValue().set(key, value, ttl, unit);
+        } catch (Exception e) {
+            log.error(key, " redis 직렬화 실패", e);
+        }
+    }
+}
