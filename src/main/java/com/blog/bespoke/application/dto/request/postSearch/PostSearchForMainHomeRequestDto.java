8d7f3c5cd3e59528a91885fe8786e87190e53272
Nam_Ssang <simian114@gmail.com>
2024-08-02 20:13:04 +0900

    [#192] chg: postSearchRequestDto 타입 메서드 추가

--- src/main/java/com/blog/bespoke/application/dto/request/postSearch/PostSearchForMainHomeRequestDto.java
+++ src/main/java/com/blog/bespoke/application/dto/request/postSearch/PostSearchForMainHomeRequestDto.java
@@ -1,13 +1,29 @@
 package com.blog.bespoke.application.dto.request.postSearch;
 
 import com.blog.bespoke.domain.model.post.PostSearchCond;
+import com.blog.bespoke.domain.service.cache.PostCacheService;
+import lombok.Setter;
 import lombok.ToString;
 
+@Setter
 @ToString
 public class PostSearchForMainHomeRequestDto implements PostSearchRequestDto {
     private Integer page;
     private Integer size;
     private PostSearchCond.OrderBy orderBy;
+
+    @Override
+    public PostCacheService.PostSearchCacheType getType() {
+        return PostCacheService.PostSearchCacheType.MAIN;
+    }
+
+    public Integer getPage() {
+        return page == null ? 0 : page;
+    }
+
+    public PostSearchCond.OrderBy getOrderBy() {
+        return orderBy == null ? PostSearchCond.OrderBy.LIKE : orderBy;
+    }
 
     public PostSearchCond toModel() {
         PostSearchCond cond = new PostSearchCond();
--- src/main/java/com/blog/bespoke/application/dto/request/postSearch/PostSearchRequestDto.java
+++ src/main/java/com/blog/bespoke/application/dto/request/postSearch/PostSearchRequestDto.java
@@ -1,9 +1,12 @@
 package com.blog.bespoke.application.dto.request.postSearch;
 
 import com.blog.bespoke.domain.model.post.PostSearchCond;
+import com.blog.bespoke.domain.service.cache.PostCacheService;
 
 public interface PostSearchRequestDto {
     String toString();
 
     PostSearchCond toModel();
+
+    PostCacheService.PostSearchCacheType getType();
 }
