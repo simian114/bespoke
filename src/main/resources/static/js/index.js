76153fa117d682ffe32daf0cd8eeb58e3565a48c
Nam_Ssang <simian114@gmail.com>
2024-08-26 23:13:32 +0900

    [#67] chg: popup 배너 존재하지 않으면 모달 띄우지 않도록 수정

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -26,3 +26,18 @@ function setCookie(name, value, days) {
     var expires = "expires=" + date.toUTCString();
     document.cookie = name + "=" + value + ";" + expires + ";path=/";
 }
+
+function saveTopBanner(e) {
+    // TODO: session storage 에 저장
+    const ret = e.detail.xhr.response;
+    sessionStorage.setItem('topBanner', ret);
+}
+
+function handleAfterOnLoad(e) {
+    const path = e.detail.pathInfo.requestPath;
+    if (path === "/banner/top-banner") {
+        //
+    } else {
+        // 추가..
+    }
+}
