1efdf6ce65815d3d1e2eb370e3f5827d47edaa59
Nam_Ssang <simian114@gmail.com>
2024-09-09 21:47:02 +0900

    [#223] fix: header 버거 클릭 안되던 문제 해결

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,25 +1,3 @@
-// NOTE: surreal 로 대체. surreal 은 LoB 에 좋음
-document.addEventListener('DOMContentLoaded', () => {
-
-    // Get all "navbar-burger" elements
-    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-
-    // Add a click event on each of them
-    $navbarBurgers.forEach(el => {
-        el.addEventListener('click', () => {
-
-            // Get the target from the "data-target" attribute
-            const target = el.dataset.target;
-            const $target = document.getElementById(target);
-
-            // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-            el.classList.toggle('is-active');
-            $target.classList.toggle('is-active');
-
-        });
-    });
-});
-
 function setCookie(name, value, days) {
     var date = new Date();
     date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000)); // days를 밀리초로 변환
